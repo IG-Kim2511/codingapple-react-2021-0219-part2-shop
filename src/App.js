5d6fbe6685a96a4bd3c7e2bc99b6ed5a2471143a
iikim <iikim2511@gmail.com>
2021-02-21 12:29:55 -0500

    {       shoes.map( function(a,i){         return(            <Card shoes={a} i={i}></Card>       )       }     )     }

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
+import React ,{useState} from 'react';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar , Nav ,NavDropdown , Form , FormControl, Button, Jumbotron} from 'react-bootstrap';
+
+import data from './data'
+
 function App() {
+
+  const [shoes, setshoes] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    <p className="black-nav">🦄15 react bootstrap (쇼핑몰 프로젝트 : 프로젝트 생성 & Bootstrap 설치)</p>
+    {/*(2)-3  */}
+
+     <div>
+
+    <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
+  
+    </div>
+
+    <div>
+    <div className="container">
+
+    <div className="row">
+      <div className="col-md-4">
+        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        <h4>{ shoes[0].title }</h4>
+        <p>{ shoes[0].content } & { shoes[0].price }</p>
+      </div>
+      <div className="col-md-4">
+        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+        <h4>상품명</h4>
+        <p>상품정보</p>
+      </div>
+      <div className="col-md-4">
+        <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+        <h4>상품명</h4>
+        <p>상품정보</p>
+      </div>
+    </div>
+
+    <br></br>
+    {
+      shoes.map( function(a,i){
+        return(
+        <div className="col-md-4">
+        <img src="https://codingapple1.github.io/shop/shoes[i].jpg" width="100%" />
+        <h4>{ shoes[i].title }</h4>
+        <p>{ shoes[i].content } & { shoes[i].price }</p>
+      </div>
+      )
+      }
+    )
+    }
+
+    <br></br>
+    <Card shoes={shoes[0]}></Card>
+    <Card shoes={shoes[1]}></Card>
+    <Card shoes={shoes[2]}></Card>
+
+    <br></br>
+    {
+      shoes.map( function(a,i){
+        return(  
+           <Card shoes={a} i={i}></Card>
+      )
+      }
+    )
+    }
+
+  </div>
+    </div>
+
+
+
+
+
+
     </div>
   );
+}
+
+function Card (p){
+  return(<div className="col-md-4">
+  <img src={"https://codingapple1.github.io/shop/shoes"+(p.i+1)+".jpg"} width="100%" />
+  <h4>{ p.shoes.title }</h4>
+  <p>{ p.shoes.content } & { p.shoes.price }</p>
+</div>
+
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+
+  
+
+
+  import React from 'react'
+  
+  const data =   [
+    {
+      id : 0,
+      title : "0-White and Black",
+      content : "0-Born in France",
+      price : 0-120000
+    },
+  
+    {
+      id : 1,
+      title : "1-Red Knit",
+      content : "1-Born in Seoul",
+      price : 1-110000
+    },
+  
+    {
+      id : 2,
+      title : "2-Grey Yordan",
+      content : "2-Born in the States",
+      price : 2-130000
+    }
+  ] 
+  
+  export default data
+  
