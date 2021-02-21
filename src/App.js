5efa345f315b72b3327907a4252daaa56c226111
iikim <iikim2511@gmail.com>
2021-02-21 13:51:41 -0500

    no message

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {Link, Route,Switch} from 'react-router-dom';
 
 
 import data from './data'
+import  Detail  from "./Detail";
 
 function App() {
 
@@ -25,8 +26,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/detail">detail</Nav.Link>
+              <Nav.Link href="/"> Home</Nav.Link>
+              <Nav.Link href="/detail"> detail</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -67,19 +68,7 @@ function App() {
         </Route>
 
         <Route path="/detail">
-            <div className="container">
-            <div className="row">
-              <div className="col-md-6">
-                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-              </div>
-              <div className="col-md-6 mt-4">
-                <h4 className="pt-5">상품명</h4>
-                <p>상품설명</p>
-                <p>120000원</p>
-                <button className="btn btn-danger">주문하기</button> 
-              </div>
-            </div>
-             </div> 
+                <Detail/>
         </Route>
         
        </div>
